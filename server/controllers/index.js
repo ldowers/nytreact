59d8c07a1cfb6d36e97ac025a0720ec69ff8e027
Lori Dowers <ldowers@cfl.rr.com>
2017-03-08 22:05:39 -0500

    Removed unused files

--- server/controllers/index.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import articleRoutes from "./articles-api.controller";
-
-const router = express.Router();
-
-router.use('/api/saved', articleRoutes);
-
-export default router;
